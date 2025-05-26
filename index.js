1e326b0b6758e63b00496191f3e5818701a8f959
ameer akech <ameerakech@gmail.com>
2025-05-26 14:27:46 +0300

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-//QUESTION ONE 
-//pseudocode
-// create a bank account withfunction that will take in name and balance
-//Add a method deposit ehich takes in an amount
+// // QUESTION ONE 
 
 function BankAccount(userName, balance){
     this.userName = userName
@@ -11,12 +8,12 @@ function BankAccount(userName, balance){
        console.log(`${this.userName} your new balance is ${this.balance}`)
     }
     
-}
 
-    this.widthdraw = function(amount){
+
+    this.withdraw = function(amount){
     if (amount <= this.balance){
         this.balance -= amount;
-        console.log(`${this.userName} withdrew ${amount}your new balance is ${this.balance}`)
+        console.log(`${this.userName} withdrew ${amount} your new balance is ${this.balance}`)
         return this.balance 
     }
     else{
@@ -24,21 +21,22 @@ function BankAccount(userName, balance){
         
     }
 }
-   const firstAccount = new BankAccount("Mary", 2000)
-   const secondAccount = new BankAccount("John", 500)
-   const thirdAccount = new BankAccount("Benson",350) 
+}
+   const firstAccount = new BankAccount("firstAccount", 2000)
+   const secondAccount = new BankAccount("secondAccount", 500)
+   const thirdAccount = new BankAccount("thirdAccount",350) 
 
     firstAccount.deposit(200)
-    firstAccount.withdraw(2100)
+    firstAccount.withdraw(2100);
 
     secondAccount.deposit(400)
     secondAccount.withdraw(300)
 
     thirdAccount.deposit(200)
     thirdAccount.withdraw(300)
-   
+console.log("=============================")   
 
-    //QUESTION TWO
+    // QUESTION TWO
 
     const taskList = {
         tasks: [],
@@ -53,6 +51,10 @@ function BankAccount(userName, balance){
           if (index > -1) {
             this.tasks.splice(index, 1);
             console.log("Completed task: " + task);
+          }
+          else {
+            console.log('Task ${task} not found ');
+            
           }
         },
       
@@ -69,7 +71,34 @@ function BankAccount(userName, balance){
       taskList.completeTask("Go hiking");
       
       taskList.listTasks();
+console.log("=======================================")
+      //  QUESTION THREE
+    function Student (name,scores){
+      this.name = name;
+      this.scores = scores;
+      this.average = function(){
+        let sum = this.scores.reduce((a,b)=> a + b,0);
+        return sum/this.scores.length
+      };
+      this.hasPassed = function(){
+        if(this.average() >= 50){
+          return true
+        }
+        else{
+          return false
+        }
+      }
+    }
+    let  student1 = new Student("John",[78,80])
+    let  student2 = new Student("Jane",[45.34])
 
+console.log(`${student1.name} has an average of: ${student1.average()}`);
+console.log(`${student1.name} has passed: ${student1.hasPassed()}`);
+console.log(`${student2.name} has an average of: ${student2.average()}`);
+console.log(`${student2.name} has passed: ${student2.hasPassed()}`);
+
+    
+console.log("============================================")
 
       // QUESTION FOUR
       //Create an variable with an empty array 
@@ -79,13 +108,11 @@ function BankAccount(userName, balance){
       
         addItem: function(item) {
           this.items.push(item);
-          console.log(`Added ${item.name} and ${item.price}to cart`)
+          console.log(`Added ${item.name} bought with ${item.price} shillings to cart`)
         },
       
        getTotal(){
-        return this.items.reduce(total,item)=>{
-            total+item.price,0
-       }
+        return this.items.reduce((total,item)=> total + item.price,0)
     
        }
     }
@@ -95,22 +122,38 @@ function BankAccount(userName, balance){
       ShoppingCart.addItem({ name: "Scissors", price: 50 });
       
       console.log("Total cost: $" + ShoppingCart.getTotal());
-
+console.log("==================================================")
 
       //QUESTION FIVE
         //Create variables that will hold the movie names and then pass the items it takes in
         //WE ass a method isClassic then pass a condition 
+
+      function Movie (title, year, rating){
+        this.title = title;
+        this.year = year;
+        this.rating = rating; 
+       this.isClassic = function(){
+      return this.year < 2000   
+        }
+      
+
+     this.isRecommend = function(){
+        if (this.rating > 8){
+            console.log(`Highly recommended!`)
+        }
+        else{
+            console.log(`Not highly recommended`)
+        }
+      }
+    }
       let firstMovie = new Movie("The Originals", 2014, 5);
       let secondMovie = new Movie("Outpost", 2023, 9);
       let thirdMovie= new Movie("Dragons", 2012, 8);
                  
-       console.log(`"${firstMovie.title}" is classic movie: ${firstMovie.isClassic()}`);
-      console.log(`Recommendation: ${firstMovie.recommend()}`);
-      
-              
-      console.log(`"${secondMovie.title}" is classic movie: ${secondMovie.isClassic()}`);
-      console.log(`Recommendation: ${secondMovie.recommend()}`);
-      
-              
-      console.log(`"${thirdMovie.title}" is classic movie: ${thirdMovie.isClassic()}`);
-      console.log(`Recommendation: ${thirdMovie.recommend()}`);
+console.log(firstMovie.isClassic());
+firstMovie.isRecommend()
+console.log(secondMovie.isClassic());
+secondMovie.isRecommend()
+console.log(thirdMovie.isClassic());
+thirdMovie.isRecommend()
+
